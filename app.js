94557124f543e72f38bb59fd17f628594fb8daae
Эмиль Чалаев <chalaevemil@yandex.ru>
2023-08-01 18:59:44 +0300

    ПР 14 v3.6

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const { PORT = 3000 } = process.env;
 const bodyParser = require("body-parser");
 const auth = require("./middlewares/auth");
 const handleError = require("./middlewares/handleError");
-const {errors} = require('celebrate')
+const { errors } = require("celebrate");
 const { login, createNewUser } = require("./controllers/users");
 const {
   createNewUserValidation,
@@ -16,21 +16,19 @@ const cookieParser = require("cookie-parser");
 const userRouter = require("./routes/users");
 const cardRouter = require("./routes/cards");
 
-
-
 const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cookieParser())
+app.use(cookieParser());
 
-app.use(userRouter);
-app.use(cardRouter);
+app.use("/users", auth, userRouter);
+app.use("/cards", auth, cardRouter);
 app.post("/signin", loginValidation, login);
 app.post("/signup", createNewUserValidation, createNewUser);
 
 app.use(auth);
-app.use(errors())
+app.use(errors());
 app.use(handleError);
 
 app.use("*", (req, res) => {
